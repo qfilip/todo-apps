0b0c55e2bf5d1e3373853ba069189de6ca0bb478
qfilip <kukolj.filip@gmail.com>
2024-10-30 23:09:05 +0100

    format fix

--- vanilla-simple/index.js
+++ vanilla-simple/index.js
@@ -1,34 +1,32 @@
 {
-	const make = (tagName, parent, attr) => parent.appendChild(
-			Object.assign(document.createElement(tagName), attr)
-		),
+	const make = (tagName, parent, attr) =>
+		parent.appendChild(Object.assign(document.createElement(tagName), attr));
 		
-		addToDo = (value) => {
+	const addToDo = (value) => {
 			const li = make("li", toDoList);
 			make("input", li, {
 				readonly : true,
 				name : "todo[]",
 				value
 			});
+			
 			make("button", li, {
 				onclick : (e) => { e.currentTarget.closest("li").remove(); },
 				textContent : "Remove",
-        type : "button"
-			});
-		},
+				type : "button"});
+		};
 		
-		form = make("form", document.querySelector("main"), {
-			onsubmit : (e) => {
-        e.preventDefault();
-        addToDo(addInput.value);
-        addInput.value = "";
-			}
-		}),
-		fieldset = make("fieldset", form),
-		addLabel = make("label", fieldset, { textContent : "Title:" }),
-		addInput = make("input", addLabel, { required : "true" }),
-		toDoList = make("ul", form);
+	form = make("form", document.querySelector("main"), {
+		onsubmit : (e) => {
+			e.preventDefault();
+			addToDo(addInput.value);
+			addInput.value = "";
+		}
+	}),
+	fieldset = make("fieldset", form),
+	addLabel = make("label", fieldset, { textContent : "Title:" }),
+	addInput = make("input", addLabel, { required : "true" }),
+	toDoList = make("ul", form);
 
 	make("button", fieldset, { textContent : "Create" });
-
 }
